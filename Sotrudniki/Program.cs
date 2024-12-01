58796f22dc5d1b4945179247ba1f4aa9abbb5c95
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2024-12-01 12:38:12 +0200

    books

---
+++ Sotrudniki/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Sotrudniki
+{
+    class Person
+    {
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public string WorkEmail { get; set; }
+
+        public override string ToString()
+        {
+            return $"ФИО: {FullName}, Должность: {Position}, Зарплата: {Salary}, Email: {WorkEmail}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> person = new List<Person>();
+            AddPerson(person, "Иван Петров", "Менеджер", 50000, "petrov@mail.com");
+            AddPerson(person, "Петр Иванов", "Разработчик", 70000, "ivanov@mail.com");
+            AddPerson(person, "Александер Александров", "3-D Дизайнер", 40000, "aleksandrov@mail.com");
+            RemovePerson(person, "Иван Петров");
+            UpdatePerson(person, "Петр Иванов", "Петр Иванович", "Старший разработчик", 80000, "ivanovich@mail.com");
+            SearchPerson(person, "Старший разработчик");
+            SortPerson(person, "Salary");
+        }
+        static void AddPerson(List<Person> person, string name, string position, decimal salary, string workemail)
+        {
+            person.Add(new Person { FullName = name, Position = position, Salary = salary, WorkEmail = workemail });
+            Console.WriteLine($"Сотрудник {name} добавлен");
+        }
+
+        static void RemovePerson(List<Person> person, string name)
+        {
+            var foundperson = person.Find(e => e.FullName == name);
+            if (foundperson != null)
+            {
+                person.Remove(foundperson);
+                Console.WriteLine($"Сотрудник {name} удален");
+            }
+            else
+            {
+                Console.WriteLine("Сотрудник не найден");
+            }
+        }
+
+        static void UpdatePerson(List<Person> person, string currentName, string newName, string newPosition, decimal newSalary, string newEmail)
+        {
+            var foundperson = person.Find(e => e.FullName == currentName);
+            if (foundperson != null)
+            {
+                foundperson.FullName = newName;
+                foundperson.Position = newPosition;
+                foundperson.Salary = newSalary;
+                foundperson.WorkEmail = newEmail;
+                Console.WriteLine($"Информация о сотруднике {newName} обновлена");
+            }
+            else
+            {
+                Console.WriteLine("Сотрудник не найден");
+            }
+        }
+
+        static void SearchPerson(List<Person> person, string query)
+        {
+            foreach (var foundperson in person)
+            {
+                if (foundperson.FullName.Contains(query) || foundperson.Position.Contains(query) || foundperson.WorkEmail.Contains(query))
+                {
+                    Console.WriteLine(foundperson);
+                }
+            }
+        }
+
+        static void SortPerson(List<Person> person, string parameter)
+        {
+            if (parameter == "Salary")
+            {
+                person.Sort((a, b) => a.Salary.CompareTo(b.Salary));
+            }
+            else if (parameter == "FullName")
+            {
+                person.Sort((a, b) => a.FullName.CompareTo(b.FullName));
+            }
+
+            Console.WriteLine("Сотрудники отсортированы:");
+            foreach (var foundperson in person)
+            {
+                Console.WriteLine(foundperson);
+            }
+        }
+    }
+}
---
+++ bookinglist/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace bookinglist
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"Название: {Title}, Автор: {Author}, Жанр: {Type}, Год: {Year}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<Book> books = new LinkedList<Book>();
+
+            AddBookToEnd(books, "1984", "Джордж Оруэлл", "Утопия", 1949);
+            AddBookToEnd(books, "Убийство Пересмешника", "Харпер Ли", "Новелла", 1960);
+            AddBookToBeginning(books, "Гордость и Предубеждение", "Джейн Остин", "Роман", 1813);
+            InsertBookAtPosition(books, 2, "Великий Гетсби", "Фрэнсис Скотт Фицджеральд", "Новелла", 1925);
+            Console.WriteLine("Список книг:");
+            DisplayBooks(books);
+            SearchBooks(books, "Оруэлл");
+            RemoveBookFromEnd(books);
+            RemoveBookFromBeginning(books);
+            RemoveBookAtPosition(books, 1);
+            Console.WriteLine("Список книг после изменений:");
+            DisplayBooks(books);
+        }
+        static void AddBookToBeginning(LinkedList<Book> books, string title, string author, string type, int year)
+        {
+            books.AddFirst(new Book { Title = title, Author = author, Type = type, Year = year });
+            Console.WriteLine($"Книга {title} добавлена в начало списка");
+        }
+        static void AddBookToEnd(LinkedList<Book> books, string title, string author, string type, int year)
+        {
+            books.AddLast(new Book { Title = title, Author = author, Type = type, Year = year });
+            Console.WriteLine($"Книга {title} добавлена в конец списка");
+        }
+        static void InsertBookAtPosition(LinkedList<Book> books, int position, string title, string author, string type, int year)
+        {
+            if (position < 0 || position > books.Count)
+            {
+                Console.WriteLine("Некорректная позиция");
+                return;
+            }
+            var newBook = new Book { Title = title, Author = author, Type = type, Year = year };
+            if (position == 0)
+            {
+                AddBookToBeginning(books, title, author, type, year);
+            }
+            else if (position == books.Count)
+            {
+                AddBookToEnd(books, title, author, type, year);
+            }
+            else
+            {
+                var current = books.First;
+                for (int i = 0; i < position - 1; i++)
+                {
+                    current = current.Next;
+                }
+                books.AddAfter(current, newBook);
+                Console.WriteLine($"Книга {title} вставлена на позицию {position}");
+            }
+        }
+        static void RemoveBookFromBeginning(LinkedList<Book> books)
+        {
+            if (books.Count > 0)
+            {
+                Console.WriteLine($"Книга {books.First.Value.Title} удалена из начала списка");
+                books.RemoveFirst();
+            }
+            else
+            {
+                Console.WriteLine("Список пуст");
+            }
+        }
+        static void RemoveBookFromEnd(LinkedList<Book> books)
+        {
+            if (books.Count > 0)
+            {
+                Console.WriteLine($"Книга {books.Last.Value.Title} удалена из конца списка");
+                books.RemoveLast();
+            }
+            else
+            {
+                Console.WriteLine("Список пуст");
+            }
+        }
+        static void RemoveBookAtPosition(LinkedList<Book> books, int position)
+        {
+            if (position < 0 || position >= books.Count)
+            {
+                Console.WriteLine("Некорректная позиция");
+                return;
+            }
+
+            var current = books.First;
+            for (int i = 0; i < position; i++)
+            {
+                current = current.Next;
+            }
+
+            Console.WriteLine($"Книга {current.Value.Title} удалена с позиции {position}");
+            books.Remove(current);
+        }
+        static void DisplayBooks(LinkedList<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        static void SearchBooks(LinkedList<Book> books, string find_book)
+        {
+            Console.WriteLine($"Результаты поиска по запросу {find_book}:");
+            foreach (var book in books)
+            {
+                if (book.Title.Contains(find_book, StringComparison.OrdinalIgnoreCase) ||
+                    book.Author.Contains(find_book, StringComparison.OrdinalIgnoreCase) ||
+                    book.Type.Contains(find_book, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(book);
+                }
+            }
+        }
+    }
+}
